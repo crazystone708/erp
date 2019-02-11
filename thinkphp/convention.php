56ee8e96cc65b0d969cfb1ae65c22d4d822d5f04
crazystone708 <crazystone708@gmail.com>
2019-02-11 20:13:22 +0800

    数据库

--- thinkphp/convention.php
+++ thinkphp/convention.php
@@ -246,13 +246,13 @@ return [
         // 服务器地址
         'hostname'        => '127.0.0.1',
         // 数据库名
-        'database'        => '',
+        'database'        => 'erp',
         // 数据库用户名
         'username'        => 'root',
         // 数据库密码
-        'password'        => '',
+        'password'        => 'Dengxiao@1219',
         // 数据库连接端口
-        'hostport'        => '',
+        'hostport'        => '3306',
         // 数据库连接参数
         'params'          => [],
         // 数据库编码默认采用utf8
